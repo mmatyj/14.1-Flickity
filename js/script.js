9f9e636297a64fe92f193d97f14311ca7111319b
Malgorzata Matyjanka <m.matyjanka@gmail.com>
2019-03-13 21:26:38 +0000

    merge branch googleMap

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+
+// GOOGLE MAP
+
+window.initMap = function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: slides[0].coords
+    });
+    for (var i = 0; i < slides.length; i++) {
+
+        var marker = new google.maps.Marker({
+            position: slides[i].coords,
+            map: map
+        });
+        marker.addListener('click', function () {
+            console.log('you clicked marker ' + (i))
+        })
+    }
+
+};
